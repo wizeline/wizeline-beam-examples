99732534df15bbea92ead1b76c697b279e6ab4b3
Hassan Reyes <hassan.reyes@wizeline.com>
2021-02-23 14:50:27 -0600

    Add ChicagoMySql pipeline

--- src/main/java/com/wizeline/examples/CdcPipelineOptions.java
+++ src/main/java/com/wizeline/examples/CdcPipelineOptions.java
@@ -1,21 +1,39 @@
 package com.wizeline.examples;
 
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableSchema;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.Validation;
 import org.apache.beam.sdk.options.ValueProvider;
 
-/** Options supported by DebeziumIO pipelines. */
+import java.util.Arrays;
+
+/**
+ * Options supported by DebeziumIO pipelines.
+ */
 public interface CdcPipelineOptions extends DataflowPipelineOptions {
 
+    public static final TableSchema TABLE_SCHEMA = new TableSchema()
+        .setFields(
+            Arrays.asList(
+                new TableFieldSchema().setName("order_number").setType("INTEGER"),
+                new TableFieldSchema().setName("order_datetime_utc").setType("DATETIME"),
+                new TableFieldSchema().setName("purchaser").setType("INTEGER"),
+                new TableFieldSchema().setName("quantity").setType("INTEGER"),
+                new TableFieldSchema().setName("product_id").setType("INTEGER"),
+                new TableFieldSchema().setName("source").setType("STRING"),
+                new TableFieldSchema().setName("billing_zipcode").setType("STRING")
+            ));
+
     @Description(
-            "The JDBC connection Hostname string.")
+        "The JDBC connection Hostname string.")
     ValueProvider<String> getHostname();
 
     void setHostname(ValueProvider<String> hostname);
 
     @Description(
-            "The JDBC connection Port string.")
+        "The JDBC connection Port string.")
     ValueProvider<String> getPort();
 
     void setPort(ValueProvider<String> port);
--- src/main/java/com/wizeline/examples/ChicagoMySqlToBigQuery.java
+++ src/main/java/com/wizeline/examples/ChicagoMySqlToBigQuery.java
@@ -1,43 +1,47 @@
 package com.wizeline.examples;
 
-import com.google.api.services.bigquery.model.TableFieldSchema;
-import com.google.api.services.bigquery.model.TableSchema;
-import com.google.cloud.bigquery.Field;
-import com.google.cloud.bigquery.Schema;
-import com.google.cloud.bigquery.StandardSQLTypeName;
-import com.google.protobuf.FieldType;
-import io.debezium.connector.postgresql.PostgresConnector;
+import com.google.api.services.bigquery.model.TableRow;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import io.debezium.connector.mysql.MySqlConnector;
 import org.apache.beam.io.debezium.DebeziumIO;
 import org.apache.beam.io.debezium.SourceRecordJson;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
-import org.apache.beam.sdk.io.gcp.bigquery.BigQueryUtils;
-import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.api.services.bigquery.model.TableRow;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
-public class LosAngelesPgSqlToBigQuery {
-    private static final Logger LOG = LoggerFactory.getLogger(LosAngelesPgSqlToBigQuery.class);
+/**
+ * Pipeline that replicates online orders from MySql using Debezium to a BigQuery table.
+ */
+public class ChicagoMySqlToBigQuery {
+    private static final Logger LOG = LoggerFactory.getLogger(ChicagoMySqlToBigQuery.class);
 
     public static void main(String[] args) {
-        CdcPipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(CdcPipelineOptions.class);
+
+        // Parse the user options passed from the command-line
+        CdcPipelineOptions options = PipelineOptionsFactory.fromArgs(args)
+            .withValidation()
+            .as(CdcPipelineOptions.class);
+
         run(options);
     }
 
     /**
      * Runs the pipeline with the supplied options.
      *
+     * @param options The execution parameters to the pipeline.
      * @return The result of the pipeline execution.
      */
     private static PipelineResult run(CdcPipelineOptions options) {
@@ -53,21 +57,23 @@ public class LosAngelesPgSqlToBigQuery {
                         DebeziumIO.ConnectorConfiguration.create()
                             .withUsername(options.getUsername())
                             .withPassword(options.getPassword())
-                            .withConnectorClass(PostgresConnector.class)
                             .withHostName(options.getHostname())
                             .withPort(options.getPort())
-                            .withConnectionProperty("database.dbname", "postgres")
-                            .withConnectionProperty("database.server.name", "server2")
-                            .withConnectionProperty("schema.include.list", "inventory")
+                            .withConnectorClass(MySqlConnector.class)
+                            .withConnectionProperty("database.server.name", "dbserver1")
                             .withConnectionProperty("database.include.list", "inventory")
                             .withConnectionProperty("table.include.list", "inventory.online_orders")
-                    .withConnectionProperty("include.schema.changes", "false")
-                    .withConnectionProperty("poll.interval.ms", "3000")
-                    ).withFormatFunction(new SourceRecordJson.SourceRecordJsonMapper()).withCoder(StringUtf8Coder.of())
-                )
+                            .withConnectionProperty("include.schema.changes", "false")
+                            .withConnectionProperty("database.allowPublicKeyRetrieval", "true")
+                            .withConnectionProperty("connect.keep.alive", "false")
+                            .withConnectionProperty("connect.keep.alive.interval.ms", "200")
+                    )
+                    .withFormatFunction(new SourceRecordJson.SourceRecordJsonMapper())
+                    .withCoder(StringUtf8Coder.of())
+            ).setCoder(StringUtf8Coder.of())
 
             //Step 2: Get record from CDC Json event, transform localtime to UTC, create a TableRow per event
-            .apply("Json to TableRow", ParDo.of(new DoFn<String, TableRow>()   {
+            .apply("Json to TableRow", ParDo.of(new DoFn<String, TableRow>() {
                 @DoFn.ProcessElement
                 public void processElement(ProcessContext c) throws Exception {
                     LOG.debug("PIPELINE STEP2: {}", c.element());
@@ -78,22 +84,30 @@ public class LosAngelesPgSqlToBigQuery {
                     tr.set("purchaser", context.read("$.after.fields.purchaser", Integer.class));
                     tr.set("quantity", context.read("$.after.fields.quantity", Integer.class));
                     tr.set("product_id", context.read("$.after.fields.product_id", Integer.class));
-                    String datetime = context.read("$.after.fields.order_local_datetime_pst");
-
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                    dateFormat.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
-                    Date order_date = dateFormat.parse(datetime);
+                    // Get date
+                    int epochDay = context.read("$.after.fields.order_date", Integer.class);
+                    long epochMillis = TimeUnit.DAYS.toMillis(epochDay);
+                    Date order_date = new Date(epochMillis);
+                    // Get time
+                    long microsecs = context.read("$.after.fields.order_time_cst", Long.class);
+                    long millis = TimeUnit.MILLISECONDS.convert(microsecs, TimeUnit.MICROSECONDS);
+                    Date order_time = new Date(millis);
+                    Calendar calendar_time = Calendar.getInstance();
+                    calendar_time.setTime(order_time);
                     // To UTC
-                    TimeZone timeZone = TimeZone.getTimeZone("America/Los_Angeles");
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.setTime(order_date);
-                    calendar.setTimeZone(timeZone);
-                    calendar.add(Calendar.HOUR, 8); // GMT-8
+                    TimeZone timeZone = TimeZone.getTimeZone("America/Chicago");
+                    Calendar calendar_date = Calendar.getInstance();
+                    calendar_date.setTime(order_date);
+                    calendar_date.add(Calendar.HOUR, calendar_time.get(Calendar.HOUR));
+                    calendar_date.add(Calendar.MINUTE, calendar_time.get(Calendar.MINUTE));
+                    calendar_date.add(Calendar.SECOND, calendar_time.get(Calendar.SECOND));
+                    calendar_date.add(Calendar.HOUR, 6); // GMT-6
+                    calendar_date.setTimeZone(timeZone);
                     SimpleDateFormat formatterWithTimeZone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                     formatterWithTimeZone.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-                    tr.set("order_datetime_utc", formatterWithTimeZone.format(calendar.getTime()));
-                    tr.set("source", "los_angeles");
+                    tr.set("order_datetime_utc", formatterWithTimeZone.format(calendar_date.getTime()));
+                    tr.set("source", "chicago");
 
                     String[] parts = context.read("$.after.fields.billing_address").toString().split(" ");
                     tr.set("billing_zipcode", parts[parts.length - 1]);
@@ -113,7 +127,7 @@ public class LosAngelesPgSqlToBigQuery {
                     .withSchema(CdcPipelineOptions.TABLE_SCHEMA)
                     .to(options.getOutputTable()));
 
+        // Execute the pipeline and return the result.
         return pipeline.run();
     }
 }
-
